0cc303783551b44aba26c9814d3f34a7ccf4d809
skaeneman <skaeneman@users.noreply.github.com>
2016-05-07 20:25:00 -0400

    Update Get_AD_Groups.sql

--- SQL_Queries/Get_AD_Groups.sql
+++ SQL_Queries/Get_AD_Groups.sql
@@ -5,8 +5,8 @@ from UserInfo
 with (nolock)
 where tp_DomainGroup = '1'
 and tp_Login != 'NT AUTHORITY\authenticated users'
-and tp_login != 'DODIIS\domain users'
+and tp_login != 'DOMAIN\domain users'
 and tp_Login != 'BUILTIN\administrators'
 and tp_Login != 'BUILTIN\users'
 and tp_Login NOT LIKE '%domain users%'
-and tp_Login NOT LIKE '%US\%'
+
